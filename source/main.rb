71574457bcc78f36c0bee6c78a9691032ffa5cb0
Jaymin Shah <shahjaymin2101@gmail.com>
2022-02-24 12:05:48 +0530

    Makes the program object oriented, and much easier to understand

--- source/main.rb
+++ source/main.rb
@@ -1,42 +1,43 @@
-def caesar_cipher(string, shift_factor)
-    string_in_char_array = string.chars
-    ciphered_array = []
-    string_in_char_array.each() do |char|
-        if is_lower_case(char)
-            ciphered_array.push(shifted_lower_case_character(char, shift_factor))
-        elsif is_upper_case(char) 
-            ciphered_array.push(shifted_upper_case_character(char, shift_factor))
-        else
-            ciphered_array.push(char)
+class CaeserCipher
+
+    def initialize(string, shift_factor)
+        @string = string
+        @shift_factor = shift_factor
+    end
+
+    def caesar_cipher()
+        string_in_char_array = @string.chars
+        ciphered_array = []
+        string_in_char_array.each() do |char|
+            if is_lower_case(char)
+                ciphered_array.push(shifted_lower_case_character(char))
+            elsif is_upper_case(char) 
+                ciphered_array.push(shifted_upper_case_character(char))
+            else
+                ciphered_array.push(char)
+            end
         end
+        return ciphered_array.join("")
     end
-    return ciphered_array.join("")
-end
 
-def is_lower_case(char)
-    return char.between?('a', 'z')
-end
-
-def is_upper_case(char)
-    return char.between?('A', 'Z')
-end
-
-def shifted_lower_case_character(char, shift_factor)
-    integer_representation_of_character = char.ord
-    integer_representation_of_character += shift_factor
-    if integer_representation_of_character > 122 
-        integer_representation_of_character -= 26
+    def is_lower_case(char)
+        return char.between?('a', 'z')
     end
-    return integer_representation_of_character.chr
-end
 
-def shifted_upper_case_character(char, shift_factor)
-    integer_representation_of_character = char.ord
-    integer_representation_of_character += shift_factor
-    if integer_representation_of_character > 90 
-        integer_representation_of_character -= 26
+    def is_upper_case(char)
+        return char.between?('A', 'Z')
     end
-    return integer_representation_of_character.chr
-end
 
-print caesar_cipher('Jaymin Shah Vadodara', 5)
+    def shifted_lower_case_character(char)
+        base = 97
+        return ((((char.ord - base) + @shift_factor) % 26) + base).chr
+    end
+
+    def shifted_upper_case_character(char)
+        base = 65
+        return ((((char.ord - base) + @shift_factor) % 26) + base).chr
+    end
+end
+    
+cc = CaeserCipher.new('Let\'s see if this works', 5)
+print cc.caesar_cipher()
